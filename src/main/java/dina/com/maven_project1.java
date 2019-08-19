2dc869dfa0ace02bddb29495d7a849c28ffb982f
dinagit2711 <dns27111@gmail.com>
2019-08-19 11:26:23 +0530

    changes done 2

---
+++ src/main/java/dina/com/maven_project1.java
@@ -0,0 +1,12 @@
+package dina.com;
+
+public class maven_project1 {
+
+    public static void main(String[] args){
+
+        for(int i=0;i<=10;i++){
+
+            System.out.println("Hello Dina this from intellij" + i++);
+        }
+    }
+}
